b9b85835971fdeae6119542e8d6777707c6e2777
Ruhshed Boymatov <stas.basov@whatever.com>
2025-03-20 09:55:50 +0700

    123

--- base/models.py
+++ base/models.py
@@ -6,10 +6,14 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание'
+    )
+    complete = models.BooleanField(default=False, verbose_name='Выполнено')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
 
     def __str__(self):
         return self.title
